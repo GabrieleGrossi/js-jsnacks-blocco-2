e3bb0765e262e3fd907f7f48b80091ad5da11b23
GabrieleGrossi <grossigabriele98@gmail.com>
2023-05-19 16:55:00 +0200

    fixed the lenguage

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let numbers = []
 let sum = 0 
 
 while(sum<50){
-    let userNumbers = parseInt(prompt('Inserisci un numero'));
+    let userNumbers = parseInt(prompt('Insert numbers'));
 
     sum += userNumbers
     numbers.push(userNumbers)
 }
 
-console.log('Numeri inseriti:', numbers)
+console.log('The numbers insert are:', numbers)
