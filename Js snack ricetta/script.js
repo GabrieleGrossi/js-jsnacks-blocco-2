7610367aeb1ec3a06b04056db9e1f2dd0f6fa52e
GabrieleGrossi <grossigabriele98@gmail.com>
2023-05-26 12:18:00 +0200

    Create js exercise

---
+++ Js snack ricetta/script.js
@@ -0,0 +1,2 @@
+// ? Dati un input e due bottoni, l'utente popola una lista di ingredienti usando il primo bottone, al termine della quale preme un secondo bottone e viene generata un'immagine casuale di un pasto al centro dello schermo dopo 2 secondi per ogni ingrediente nella ricetta..
+
